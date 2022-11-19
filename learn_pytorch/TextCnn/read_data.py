1203ef43406d7a697556bcf0b677084ca33db330
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 08:19:32 +0000

    Adding tarfile member sanitization to extractall()

--- learn_pytorch/TextCnn/read_data.py
+++ learn_pytorch/TextCnn/read_data.py
@@ -4,7 +4,26 @@ import random
 def decompression(file_path, file_name):
     fname = os.path.join(file_path, file_name)
     with tarfile.open(fname, 'r') as f:
-        f.extractall(file_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f, file_path)
 
 def read_calimdb(folder='train', data_root = '../../data/aclImdb'):
     result = []
